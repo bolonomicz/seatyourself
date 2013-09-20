1d2ae170683e0146de2a01905c5c41c3b0548a35
Ralph Bolo <ralph.bolo@gmail.com>
2013-09-19 20:00:58 -0400

    create customer model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,4 +4,5 @@ class Restaurant < ActiveRecord::Base
   validates :name, :address, :phone_number, presence: :true
 	validates :owner_id, presence: true
   belongs_to :owner
+  mount_uploader :image, ImageUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130918223724) do
+ActiveRecord::Schema.define(:version => 20130920000010) do
+
+  create_table "customers", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+  end
 
   create_table "owners", :force => true do |t|
     t.string   "name"
